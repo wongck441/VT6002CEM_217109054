ff9d736a67032923d9bcff511b7332797db134a3
Nobel <wongck4411@gmail.com>
2021-12-31 01:11:14 +0800

    Initial Commit

---
+++ VT6002CEM_217109054/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VT6002CEM_217109054
+//
+//  Created by User on 31/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
