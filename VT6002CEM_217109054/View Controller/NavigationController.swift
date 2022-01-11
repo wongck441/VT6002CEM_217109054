97602522a1c76eff4aa01247c3e8cb6c73ee0800
Nobel <wongck4411@gmail.com>
2022-01-11 18:53:12 +0800

    add the new navigation controller swift file

---
+++ VT6002CEM_217109054/View Controller/NavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationController.swift
+//  VT6002CEM_217109054
+//
+//  Created by User on 11/1/2022.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
