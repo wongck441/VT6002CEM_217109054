7138a7d5efe24dba0edfb2b097efbb5cb56403f7
Nobel <wongck4411@gmail.com>
2022-01-12 02:56:33 +0800

    add the location point

--- VT6002CEM_217109054/View Controller/MapKitController.swift
+++ VT6002CEM_217109054/View Controller/MapKitController.swift
@@ -12,11 +12,41 @@ import Firebase
 
 class MapKitController: UIViewController,CLLocationManagerDelegate{
 
+    //connect map view
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     
-    override func viewDidLoad() {
-            super.viewDidLoad()
+    //add the location lat and lon
+        var annotations = [MKPointAnnotation]();
+        override func viewDidLoad() {
+         super.viewDidLoad()
+         let Annotation = MKPointAnnotation();
+         Annotation.coordinate = CLLocationCoordinate2D(latitude: 22.33110107335519, longitude: 114.15992503948696);
+         Annotation.title = "Leung So Kee Umbrella";
+         self.annotations.append(Annotation);
+            
+            let Annotation2 = MKPointAnnotation();
+            Annotation2.coordinate = CLLocationCoordinate2D(latitude: 22.330852878125494, longitude: 114.1634119511545);
+            Annotation2.title = "Hop Hing Umbrella";
+            self.annotations.append(Annotation2);
+            
+            let Annotation3 = MKPointAnnotation();
+            Annotation3.coordinate = CLLocationCoordinate2D(latitude: 22.314161950770153, longitude: 114.2255042884162);
+            Annotation3.title = "Sam Hung Hong";
+            self.annotations.append(Annotation3);
+            
+            let Annotation4 = MKPointAnnotation();
+            Annotation4.coordinate = CLLocationCoordinate2D(latitude: 22.329833713037363, longitude: 114.16403198546008);
+            Annotation4.title = "sunrise1842";
+            self.annotations.append(Annotation4);
+            
+            let Annotation5 = MKPointAnnotation();
+            Annotation5.coordinate = CLLocationCoordinate2D(latitude: 22.312661234240014, longitude: 114.22197836934485);
+            Annotation5.title = "Tung Sing Umbrella (Hong Kong) Enterprise Company";
+            self.annotations.append(Annotation5);
+         //show umbrella location here
+
+         self.mapView?.addAnnotations(self.annotations);
     }
     override func viewDidAppear(_ animated:Bool){
         super.viewDidAppear(animated)
@@ -41,7 +71,7 @@ class MapKitController: UIViewController,CLLocationManagerDelegate{
         
         mapView.setRegion(region, animated: true)
         
-        let pin = MKPointAnnotation()
+        let pin = MKPointAnnotation()//show the point
         pin.coordinate = coordinate
         mapView.addAnnotation(pin)
     }
