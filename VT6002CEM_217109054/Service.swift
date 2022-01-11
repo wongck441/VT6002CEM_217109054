e000aa5ed1cb16917fbcd213a11b6b4c2d53f12b
Nobel <wongck4411@gmail.com>
2022-01-11 18:53:53 +0800

    delete the get user info function

--- VT6002CEM_217109054/Service.swift
+++ VT6002CEM_217109054/Service.swift
@@ -7,32 +7,9 @@
 
 import Foundation
 import UIKit
-import Firebase
 
 class Service {
-    static func getUserInfo(onSuccess: @escaping () -> Void, onError: @escaping (_ error: Error?) -> Void){
-     let ref = Database.database().reference()
-     let defaults = UserDefaults.standard
-     
-     guard let uid = Auth.auth().currentUser?.uid else {
-     print("User not found")
-     return
-     }
-     
-     ref.child("users").child(uid).observe(.value, with: { (snapshot) in
-      if let dictionary = snapshot.value as? [String : Any] {
-         let email = dictionary["email"] as! String
-         let name = dictionary["name"] as! String
-         
-         defaults.set(email, forKey: "userEmailKey")
-         defaults.set(name, forKey: "userNameKey")
-         
-         onSuccess()
-         }
-         }){ (error) in
-            onError(error)
-            }
-            }
+    
     
     static func createAlertController(title: String, message: String)-> UIAlertController {
         let alert = UIAlertController (title: title, message: message, preferredStyle: .alert)
